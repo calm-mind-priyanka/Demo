51ff164e7679f857a121abfb56c9bda3eef0866f
calm-mind-priyanka <priyankagogoi1241@gmail.com>
2025-06-12 16:01:02 +0530

    Add files via upload

--- info.py
+++ info.py
@@ -3,29 +3,34 @@ from os import environ
 
 id_pattern = re.compile(r'^.\d+$')
 
+# Bot information
 SESSION = environ.get('SESSION', 'TechVJBot')
 API_ID = int(environ.get('API_ID', '24222039'))
 API_HASH = environ.get('API_HASH', '6dd2dc70434b2f577f76a2e993135662')
 BOT_TOKEN = environ.get('BOT_TOKEN', "")
 
+# Bot settings
 PORT = environ.get("PORT", "8080")
+
+# Online Stream and Download
 MULTI_CLIENT = False
 SLEEP_THRESHOLD = int(environ.get('SLEEP_THRESHOLD', '60'))
-PING_INTERVAL = int(environ.get("PING_INTERVAL", "1200"))
-ON_HEROKU = 'DYNO' in environ
+PING_INTERVAL = int(environ.get("PING_INTERVAL", "1200"))  # 20 minutes
+if 'DYNO' in environ:
+    ON_HEROKU = True
+else:
+    ON_HEROKU = False
 URL = environ.get("URL", "")
 
+# Admins, Channels & Users
 LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1002433610423'))
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '6046055058').split()]
-OWNER_ID = int(environ.get('OWNER_ID', '6046055058'))
 
+# MongoDB information
 DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://Rpsing:Rpsing2003@rpsing.thqdyo6.mongodb.net/?retryWrites=true&w=majority&appName=Rpsing")
 DATABASE_NAME = environ.get('DATABASE_NAME', "techvjautobot")
 
-SHORTLINK = bool(environ.get('SHORTLINK', False))
+# Shortlink Info
+SHORTLINK = bool(environ.get('SHORTLINK', False)) # Set True Or False
 SHORTLINK_URL = environ.get('SHORTLINK_URL', 'api.shareus.io')
 SHORTLINK_API = environ.get('SHORTLINK_API', 'hRPS5vvZc0OGOEUQJMJzPiojoVK2')
-
-PREMIUM_FILE = "premium_users.json"
-USAGE_FILE = "daily_usage.json"
-F_SUB_FILE = "force_sub_channels.json"
