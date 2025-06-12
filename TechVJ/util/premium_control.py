65960d2d726342d81c1c2239039f440540d66ab3
calm-mind-priyanka <priyankagogoi1241@gmail.com>
2025-06-12 12:24:01 +0530

    Update premium_control.py

--- TechVJ/util/premium_control.py
+++ TechVJ/util/premium_control.py
@@ -1,25 +1,11 @@
-import datetime
-from info import ADMINS
-from database.users_chats_db import db
-from database.usage_db import UsageDB  # This should be defined in your usage_tracker.py
-
-usage_db = UsageDB()
-
-async def is_premium(user_id: int) -> bool:
-    return user_id in ADMINS  # You can modify this to check from DB if you want dynamic premium list
+from info import PREMIUM_USERS
+from database.usage_db import UsageDB
 
 async def can_generate_link(user_id: int) -> bool:
-    if await is_premium(user_id):
+    if user_id in PREMIUM_USERS:
         return True
+    return UsageDB.can_generate(user_id)
 
-    last_use = await usage_db.get_last_usage(user_id)
-    if not last_use:
-        await usage_db.update_usage(user_id)
-        return True
-
-    now = datetime.datetime.now()
-    if (now - last_use).days >= 1:
-        await usage_db.update_usage(user_id)
-        return True
-
-    return False
+def increment_usage(user_id: int):
+    if user_id not in PREMIUM_USERS:
+        UsageDB.increment_usage(user_id)
