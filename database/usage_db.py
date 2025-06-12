b330dfe8bbc0073d74bb4c6fca1339dfba3f9d3e
calm-mind-priyanka <priyankagogoi1241@gmail.com>
2025-06-12 10:56:08 +0530

    Create usage_db.py

---
+++ database/usage_db.py
@@ -0,0 +1,22 @@
+import datetime
+
+class UsageDB:
+    usage_data = {}
+
+    @classmethod
+    def can_generate(cls, user_id):
+        today = datetime.date.today().isoformat()
+        user_data = cls.usage_data.get(user_id, {})
+
+        if user_data.get("date") != today:
+            cls.usage_data[user_id] = {"date": today, "count": 0}
+
+        return user_data.get("count", 0) < 1  # Limit to 1 link/day
+
+    @classmethod
+    def increment_usage(cls, user_id):
+        today = datetime.date.today().isoformat()
+        if user_id not in cls.usage_data or cls.usage_data[user_id]["date"] != today:
+            cls.usage_data[user_id] = {"date": today, "count": 1}
+        else:
+            cls.usage_data[user_id]["count"] += 1
